aa1264a90e57ade412c9fc5b543fc59576bcfe5b
unknown <aribahtiar@unsil.ac.id>
2020-09-27 23:20:37 +0700

    up to jpg

--- app/Http/Controllers/qnaController.php
+++ app/Http/Controllers/qnaController.php
@@ -225,7 +225,7 @@ class qnaController extends Controller
             
                     $image = str_replace(' ', '+', $image); 
             
-                    $imageName = Auth::id() ."/". time().'.png';
+                    $imageName = Auth::id() ."/". time().'.jpg';
             
                     Storage::disk('public')->put($imageName, base64_decode($image));
 
