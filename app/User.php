d22384865ba84e39a0ff309a15fc03570c8930f6
unknown <aribahtiar@unsil.ac.id>
2020-10-23 17:47:53 +0700

    upp

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',"admin","whatsapp","fb_id","email"
+        'password', 'remember_token',"admin","fb_id","email"
     ];
 
     /**
